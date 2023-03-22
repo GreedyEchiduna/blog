eee06ecc44dda23b1667258fc02c5c9628548f40
Greedy Echiduna <martinwittgenstein0424@gmail.com>
2023-03-22 19:01:10 +0800

    继续修改了部分元信息 在IDEA中打上了书签 将README.md 中的部分内容转移到了参考手册中

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,13 +2,13 @@ const siteMetadata = {
   title: 'EchidunaBlog',
   author: 'Echiduna',
   headerTitle: 'EchidunaBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  description: 'A Blog inspired by Tails Azimuth',
+  language: 'zh-cn',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/avatar.png', // 这个是作者头像
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
   github: 'https://github.com',
@@ -16,7 +16,8 @@ const siteMetadata = {
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  locale: 'zh-CN',
+  // 这里是评论, 用户信和网页分析的元信息, 暂时不能删除
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
