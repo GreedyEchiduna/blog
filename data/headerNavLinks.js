d60cd07a83f1e0ba26a05bb74e8ce05dc81cb639
Greedy Echiduna <martinwittgenstein0424@gmail.com>
2023-03-23 01:11:18 +0800

    1. 修改了博客右侧导航栏 2. 进一步完善使用参考手册

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,6 @@
+// 这个文件内定义了标题栏右侧的导航链接
 const headerNavLinks = [
-  { href: '/blog', title: '博文' },
+  { href: '/blog', title: '博文搜索' },
   { href: '/tags', title: '标签' },
   // { href: '/projects', title: '项目' },
   { href: '/about', title: '关于我' },
