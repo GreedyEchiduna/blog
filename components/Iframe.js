124f38637cccf652596c619a29e30a62b8646759
Greedy Echiduna <martinwittgenstein0424@gmail.com>
2023-03-24 14:49:22 +0800

    update spotify test

--- components/Iframe.js
+++
@@ -1,9 +0,0 @@
-const Iframe = () => {
-    return (
-        <>
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3yZEUIdqCvuPvxVXWETXBL?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </>
-    );
-};
-
-export default Iframe;
