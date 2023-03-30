7d129cac7f9c351c15f9739de0192743593147e4
Greedy Echiduna <martinwittgenstein0424@gmail.com>
2023-03-30 11:22:30 +0800

    更新博文

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,8 +2,9 @@
 const headerNavLinks = [
   { href: '/blog', title: '博文搜索' },
   { href: '/tags', title: '标签' },
-  // { href: '/projects', title: '项目' },
+  // { href: 'music', title: '音乐' },
   { href: '/about', title: '关于我' },
+  // { href: '/projects', title: '项目' },
 ]
 
 export default headerNavLinks
